66f5ad8ba491ee6380d4aeb8420cc1aec57208af
Andrew Grachev <dr.grachev@gmail.com>
2023-03-11 00:57:53 +0300

    Commit 2

--- PostgreSQL/basebackup.sh
+++ PostgreSQL/basebackup.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-pg_basebackup -D - -Ft -X fetch -d postgresql://postgres:12345678@localhost | bzip2 > /tmp/dump.tar.bz2
+# Создание бекапа дазы PostgreSQL
+pg_basebackup -D - -Ft -X fetch -d postgresql://postgres:12345678@localhost | bzip2 > /tmp/pgbackup.tar.bz2
